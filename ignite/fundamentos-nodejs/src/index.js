82868cbc1345a28bdb10404e1257dfcaa258067e
Hugo Carmona <hugoc@itflex.com.br>
2021-07-28 17:53:11 -0300

    study on how to work with the infos in the methods

--- ignite/fundamentos-nodejs/src/index.js
+++ ignite/fundamentos-nodejs/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {uuid} = require('uuidv4')
 
 const app = express()
 
@@ -21,47 +22,56 @@ app.use(express.json())
  * Request Body: Conteúdo na hora de criar ou editar um recurso
  */
 
+const projects = []
+
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query
+  const { title} = request.query
 
-  console.log(title)
-  console.log(owner)
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ])
+  return response.json(results)
 })
 
 app.post('/projects', (request, response) => {
-  const body = request.body
+  const { title, owner } = request.body
 
-  console.log(body)
+  const project = { id: uuid(), title, owner}
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  projects.push(project)
+
+  return response.json(project)
 })
 
 app.put('/projects/:id', (request, response) => {
   const {id} = request.params
+  const { title, owner } = request.body
+  const project = { id, title, owner}
 
-  console.log(`id: ${id}`)
+  const projectIndex = projects.findIndex(project => project.id === id)
 
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: 'Project Not Found'})
+  } else {
+    projects[projectIndex] = project
+
+    return response.json(project)
+  }
 })
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  const {id} = request.params
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: 'Project Not Found'})
+  } else {
+    projects.splice(projectIndex, 1)
+
+    return response.status(204).send()
+  }
 })
 
 //localhost:3333
